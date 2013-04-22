2f94868fc0f8e27d0d1eee1e220a43d5f78f8046
sanem <sanemucan@gmail.com>
2013-04-22 00:48:07 -0300

    create Node.java

---
+++ CatsDogs/Node.java
@@ -0,0 +1,19 @@
+package CatsDogs;
+
+public class Node {
+
+  String name;
+	int weight;
+	
+	public Node(String n) {
+		// /TODO Auto-generated constructor stub
+		this.name = n;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	
+
+}
