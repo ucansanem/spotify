7f7ee21e7ea4bef229f9a09d309aefbc0f6d0f05
sanem <sanemucan@gmail.com>
2013-04-22 00:40:37 -0300

    ReverseBit in Java
    Yi has moved to Sweden and now goes to school here. The first years of schooling she got in China, and the curricula do not match completely in the two countries. Yi likes mathematics, but now... The teacher explains the algorithm for subtraction on the board, and Yi is bored. Maybe it is possible to perform the same calculations on the numbers corresponding to the reversed binary representations of the numbers on the board? Yi dreams away and starts constructing a program that reverses the binary representation, in her mind. As soon as the lecture ends, she will go home and write it on her computer.
    
    Task
    Your task will be to write a program for reversing numbers in binary. For instance, the binary representation of 13 is 1101, and reversing it gives 1011, which corresponds to number 11.
    
    Input
    The input contains a single line with an integer N, 1 ≤ N ≤ 1000000000.
    
    Output
    Output one line with one integer, the number we get by reversing the binary representation of N.
    
    Sample input 1
    13
    Sample output 1
    11
    Sample input 2
    47
    Sample output 2
    61

---
+++ ReverseBit.java
@@ -0,0 +1,48 @@
+
+public class ReverseBit {
+
+  /**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter the number");
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		if(i < 1 || i > 1000000000){
+			System.out.println("Number should be between 1 and 1000000000");
+		}
+		else{
+			bitConvert(i);
+		}
+		
+	}
+
+	private static void bitConvert(int i) {
+		// TODO Auto-generated method stub
+		String s = "";
+		int div = i;
+		while( div != 0 ){
+			if( div%2 == 0 ){
+				s += "0";
+			}else{
+				s += "1";
+			}
+			
+			div = div/2;
+		}
+		
+		int res = 0;
+		
+		char[] ar = s.toCharArray();
+		int len = ar.length;
+		for(int a = 0; a < len ; a++){
+
+			if(ar[a] == '1'){
+				res += Math.pow(2, (len-1-a));
+			}
+		}
+		System.out.println(res);
+	}
+
+}
